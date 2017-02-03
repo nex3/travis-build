f28a751ae6e7e9195219f46ab9c9bdef0e33b63f
Hiro Asari <asari.ruby@gmail.com>
2017-02-02 20:29:12 -0500

    Dump .build_stages

--- lib/travis/build/stages.rb
+++ lib/travis/build/stages.rb
@@ -62,7 +62,7 @@ module Travis
           names.each { |name| run_stage(type, name) }
         end
 
-        sh.raw "source $HOME/.build_stages"
+        sh.raw "cat $HOME/.build_stages"
 
         stages.each_slice(2).each do |type, names|
           names.each do |stg|
